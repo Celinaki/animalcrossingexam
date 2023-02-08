edda7369fb96488cf5385df7f3e4391d44f4d195
Celinaki <celina1999@live.se>
2023-02-08 23:35:43 +0100

    Implemented pause function, volume function and duration of the song

--- src/components/Songcard.jsx
+++ src/components/Songcard.jsx
@@ -1,45 +1,66 @@
 import useSound from 'use-sound';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import { Howl } from 'howler';
 
 const Songcard = (song)=>{
     const thesong = song.song;
-    const [songIsPlaying,setSongIsPlaying ] = useState(false)
 
-    const audi = new Audio(thesong.music_uri)
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [audio, setAudio] = useState(new Audio(thesong.music_uri));
+    const [currentTime, setCurrentTime] = useState(0);
+    const [duration, setDuration] = useState(0);
+    const [volume, setVolume] = useState(1);
 
-    function playSong(){
+    useEffect(() => {
+        audio.addEventListener('loadedmetadata', () => {
+          setDuration(audio.duration);
+        });
+    
+        audio.addEventListener('timeupdate', () => {
+          setCurrentTime(audio.currentTime);
+        });
+    
+        return () => {
+          audio.removeEventListener('loadedmetadata', () => {});
+          audio.removeEventListener('timeupdate', () => {});
+        };
+      }, [audio]);
 
-        if(songIsPlaying === false){
-        setSongIsPlaying(true)
-        audi.play()
-            setSongIsPlaying(true)
+    const togglePlay = () => {
+        if (isPlaying) {
+          audio.pause();
+        } else {
+          audio.play();
         }
-        else{
-        audi.pause()            
-        setSongIsPlaying(false)
-        
-        }
-         
-    }
-    const [ playSound ] = useSound(thesong.music_uri)
+        setIsPlaying(!isPlaying);
+      };
 
-    const playTheSong=(src)=>{
-        const music = new Howl({
-            src,
-            html5:true
-        })
-  
-   return music.playing() ? music.pause() : music.play();    }
+      const handleSeek = (e) => {
+        audio.currentTime = e.target.value;
+      };
+    
+      const handleVolume = (e) => {
+        setVolume(e.target.value);
+        audio.volume = e.target.value;
+      };
 
 
     return(
         <div>
             <h1>{thesong.name["name-USen"]}</h1>
-            <h1 onClick={() => playSong()}>play sound</h1>
-            <button onClick={()=> playTheSong(thesong.music_uri)}>
-                Play!
-            </button>
+            <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
+            <p>
+        {Math.floor(currentTime)} / {Math.floor(duration)}
+      </p>
+      <input
+        type="range"
+        min={0}
+        max={duration}
+        value={currentTime}
+        onChange={handleSeek}
+      />
+      <p>Volume: {volume}</p>
+      <input type="range" min={0} max={1} step={0.01} value={volume} onChange={handleVolume} />
         </div>
     )
 }
