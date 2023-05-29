f0febd353f9698d15ee8acad69cef1caec6bd065
Celinaki <celina1999@live.se>
2023-05-29 16:39:00 +0200

    Reloading page if click comes from same page user is already on, in categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -30,31 +30,46 @@ const fetchChosen = (q) => {
 //     }
  }
 
+ const reloadPage= ( ) =>{
+  if (window.location.pathname === '/villagers' || window.location.pathname === '/') {
+    window.location.reload();
+  }
+  else if (window.location.pathname === '/songs' ) {
+    window.location.reload();
+  }
+  else if (window.location.pathname === '/seacreatures' ) {
+    window.location.reload();
+  }
+  else if (window.location.pathname === '/bugs' ) {
+    window.location.reload();
+  }
+ }
+
     return (
         <div className={style.categorieswrap}>
 <ul className={style.listitems}>
    <NavLink to={`/villagers`}
       style={({ isActive }) => ({ background: isActive ? 'rgb(222 184 135 / 61%)' : '' })}>
      <li 
-     onClick={()=>{fetchChosen('villagers'); }}>
+     onClick={()=>{fetchChosen('villagers'); reloadPage()}}>
         - VILLAGERS -
     </li>
     </NavLink>
     <NavLink to={`/songs`}
       style={({ isActive }) => ({ background: isActive ? 'rgb(222 184 135 / 61%)' : '' })}>
-    <li onClick={()=>{fetchChosen('songs')}}>
+    <li onClick={()=>{fetchChosen('songs');reloadPage()}}>
         - SONGS -
     </li>    
     </NavLink>
     <NavLink to={`/seacreatures`}
       style={({ isActive }) => ({ background: isActive ? 'rgb(222 184 135 / 61%)' : '' })}>
-    <li onClick={()=>{fetchChosen('seacreatures')}}>
+    <li onClick={()=>{fetchChosen('seacreatures');reloadPage()}}>
         - SEARCREATURES -
     </li>
     </NavLink>
     <NavLink to={`/bugs`}
       style={({ isActive }) => ({ background: isActive ? 'rgb(222 184 135 / 61%)' : '' })}>
-    <li onClick={()=>{fetchChosen('bugs')}}>
+    <li onClick={()=>{fetchChosen('bugs');reloadPage()}}>
         - BUGS -
     </li>
     </NavLink>
